f8e3b1eb22b73b9e1d034254a33258a257de25d5
Corey Angelus <corey.angelus@accenture.com>
2019-10-11 21:13:39 -0400

    add genus migration

---
+++ server/migrations/201910112106590-create-genus.js
@@ -0,0 +1,55 @@
+const { Level, Frequency } = require('../utils/enums.ts');
+
+module.exports = {
+  up: async (queryInterface, {
+    UUID, DATE, STRING, ENUM, literal
+  }) => {
+   return queryInterface.createTable('genera', {
+      id: {
+        type: UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: literal('uuid_generate_v4()'),
+      },
+      createdAt: {
+        allowNull: false,
+        type: DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DATE
+      },
+      nickname: {
+        allowNull: false,
+        type: STRING
+      },
+      nomenclature: {
+        allowNull: false,
+        type: STRING
+      },
+      description: {
+        allowNull: false,
+        type: STRING
+      },
+      instructions: {
+        allowNull: false,
+        type: STRING
+      },
+      photo: {
+        allowNull: false,
+        type: STRING
+      },
+      luxPreferred: {
+        allowNull: false,
+        type: ENUM([...Level])
+      }, 
+      wateringCycle: {
+        allowNull: false,
+        type: ENUM([...Frequency])
+      }, 
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('genera');
+  }
+};
