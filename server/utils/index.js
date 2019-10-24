4e70cddb30b6ba291385dbe24106e6380e80bf66
Corey Angelus <corey.angelus@accenture.com>
2019-10-24 16:51:04 -0400

    configure for heroku deployment

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,7 +1,5 @@
 module.exports = {
   raiseNotFoundError: require('./raise-not-found-error'),
-  getBuildNumber: require('./get-build-number'),
-  updateBuildNumber: require('./update-build-number'),
   getRandomId: require('./getRandomId'),
   enums: require('./enums')
 }
