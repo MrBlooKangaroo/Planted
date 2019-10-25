037fbcb63d45f75ddaf6694ad0a5253d21ecdb46
Corey Angelus <corey.angelus@accenture.com>
2019-10-25 00:22:54 -0400

    create top bar elements

--- client/src/components/home-page/root/index.jsx
+++ client/src/components/home-page/root/index.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import styles from './styles.css';
-import { Login } from '../../login/root/index';
-// import styles from './styles.css';
+import React, { Fragment } from 'react';
 import NavBar from '../NavBar';
 import Explore from '../Explore';
 
 const HomePageRoot = () => (
-  <main className={styles.root}>
-    <h1 className={styles.headline}>Planted</h1>
+  <Fragment>
     <NavBar />
-  </main>
+    <Explore />
+  </Fragment>
 );
 
 export { HomePageRoot };
