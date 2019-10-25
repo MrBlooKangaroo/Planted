be3dfd53b8c24e7eb1188e3755fdafd3f5043e9f
Corey Angelus <corey.angelus@accenture.com>
2019-10-25 00:22:54 -0400

    create top bar elements

--- client/src/components/home-page/root/index.jsx
+++ client/src/components/home-page/root/index.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import styles from './styles.css';
-import { Login } from '../../login/root/index';
+import React from 'react'
+import styles from './styles.css'
+import { Login } from '../../login/root/index'
+// import styles from './styles.css';
 
 export const text = {
-  headline: 'Graphql Starter Kit',
-  greeting: 'Make some stuff!',
-};
+	headline: 'Graphql Starter Kit',
+	greeting: 'Make some stuff!'
+}
 
 const HomePageRoot = () => (
-  <main className={styles.root}>
-    <h1 className={styles.headline}>{text.headline}</h1>
-    <section>
-      <span className={styles.greeting}>{text.greeting}</span>
-      <Login />
-    </section>
-  </main>
-);
+	<main className={styles.root}>
+		<h1 className={styles.headline}>{text.headline}</h1>
+		<section>
+			<span className={styles.greeting}>{text.greeting}</span>
+			<Login />
+		</section>
+	</main>
+)
 
-export { HomePageRoot };
+export { HomePageRoot }
