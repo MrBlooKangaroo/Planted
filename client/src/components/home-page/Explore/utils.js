df45105bed54e0c03b2d3c0c0873ca7e0dfc7e1b
Corey Angelus <corey.angelus@accenture.com>
2019-10-26 16:40:12 -0400

    add dropdown menu

--- client/src/components/home-page/Explore/utils.js
+++ client/src/components/home-page/Explore/utils.js
@@ -1,11 +1,11 @@
 export const vibes = [
-  'Fresh & Airy',
-  'Jungle Chic',
-  'Monster',
-  'Forest Fun',
-  'On the Bayou',
-  'Greenhouse',
-  "Farmer's Fantasy",
-];
+	'Fresh & Airy',
+	'Jungle Chic',
+	'Monster',
+	'Forest Fun',
+	'On the Bayou',
+	'Greenhouse',
+	"Farmer's Fantasy"
+]
 
-export const plantTypes = ['Monstera', 'Ficus', "Elephant's Ear"];
+export const plantTypes = [ 'Monstera', 'Ficus', "Elephant's Ear" ]
