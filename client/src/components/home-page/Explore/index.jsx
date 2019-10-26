01a9fea6e209272d158e63c9f00d3f26fdd15afd
Corey Angelus <corey.angelus@accenture.com>
2019-10-26 16:40:12 -0400

    add dropdown menu

--- client/src/components/home-page/Explore/index.jsx
+++ client/src/components/home-page/Explore/index.jsx
@@ -1,16 +1,37 @@
-import React, { Fragment } from 'react';
-import { header, dropdown, caret } from './styles.css';
+import React, { Fragment, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { vibes } from './utils';
+import {
+  header,
+  chooseVibe,
+  caretUp,
+  caretDown,
+  menuOpen,
+  menuClosed,
+  menuItem,
+} from './styles.css';
 
-const Explore = () => (
-  <Fragment>
-    <main className={header}>Find new plant friends.</main>
-    <div className={dropdown}>
-      CHOOSE YOUR VIBE
-      <FontAwesomeIcon className={caret} icon={faAngleDown} />
-    </div>
-  </Fragment>
-);
+const Explore = () => {
+  const [isOpen, toggle] = useState(false);
+  return (
+    <Fragment>
+      <main className={header}>Find new plant friends.</main>
+      <div className={chooseVibe}>
+        CHOOSE YOUR VIBE
+        <FontAwesomeIcon
+          icon={faAngleDown}
+          className={isOpen ? caretDown : caretUp}
+          onClick={() => toggle(!isOpen)}
+        />
+      </div>
+      <ul className={isOpen ? menuOpen : menuClosed}>
+        {vibes.map(vibe => (
+          <li className={menuItem}>{vibe}</li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
 
 export default Explore;
