bc88a183d11a90512fe2675702a7a1623e27d451
Corey Angelus <corey.angelus@accenture.com>
2019-10-28 11:52:00 -0400

    refine watering seed code

--- server/seeders/20191022103131-seed-waterings.js
+++ server/seeders/20191022103131-seed-waterings.js
@@ -3,27 +3,29 @@ const moment = require('moment')
 
 module.exports = {
   up: async () => {
-    let expectedAt
     const plants = await db.plant.findAll()
-      plantTypes = await db.plantType.findAll()
-      wateringSeeds = plants.map(plant => {
-      plantTypes.forEach(plantType => {
-        if (plant.plantTypeId === plantType.id) {
-          switch(plantType.waterCycle) {
-            default:
-            case 'WEEKLY': 
-              expectedAt = moment().add(1, 'weeks').toISOString()
-              break
-            case 'BIWEEKLY':
-              expectedAt = moment().add(2, 'weeks').toISOString()
-              break
-            case 'MONTHLY':
-              expectedAt = moment().add(1, 'months').toISOString()
-              break
+    const plantTypes = await db.plantType.findAll()
+    const wateringSeeds = plants.map(plant => {
+      const waterCycle = plantTypes.find(({ id }) =>
+        id === plant.plantTypeId
+      ).waterCycle
+      switch(waterCycle) {
+        case 'WEEKLY': 
+          return { 
+            expectedAt: moment().add(1, 'weeks').toISOString(),
+            plantId: plant.id
           }
-        }
-      })
-      return { expectedAt, plantId: plant.id }
+        case 'BIWEEKLY':
+          return { 
+            expectedAt: moment().add(2, 'weeks').toISOString(),
+            plantId: plant.id
+          }
+        case 'MONTHLY':
+          return { 
+            expectedAt: moment().add(1, 'weeks').toISOString(),
+            plantId: plant.id
+          }
+      }
     })
     await db.watering.bulkCreate(wateringSeeds)
   },
