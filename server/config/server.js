5d469fe63f42b3f28b22d3b2da0e465066bc5776
Corey Angelus <corey.angelus@accenture.com>
2019-10-29 15:02:49 -0400

    revive isIntrospectionOn logic

--- server/config/server.js
+++ server/config/server.js
@@ -13,12 +13,17 @@ const context = async ({ _req, connection }) =>
 
 const onConnect = async (connectionParams) => true
 
+const isIntrospectionOn = 
+	process.env.NODE_ENV !== 'production' ||
+	(process.env.NODE_ENV === 'production' 
+	&& process.env.IS_INTROSPECTION_ON === 'true')
+
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 	context,
-	introspection: true,
-	playground: true,
+	introspection: isIntrospectionOn,
+	playground: isIntrospectionOn,
 	subscriptions: { onConnect }
 })
 
