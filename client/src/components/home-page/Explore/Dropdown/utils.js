8cb8ee7ff4d2dc4b80035b95e4852e6a5d0d7f61
Corey Angelus <corey.angelus@accenture.com>
2019-10-29 14:56:18 -0400

    move dropdown into separate component folder

---
+++ client/src/components/home-page/Explore/Dropdown/utils.js
@@ -0,0 +1,17 @@
+export const categories = [
+  'Airy and Fresh',
+  'Herbs',
+  'Daydream',
+  'Dark and Moody',
+  'Monsters',
+  'Botanical Garden',
+  'Prehistoric',
+  'Bold Branches',
+  'Indie',
+  'French Bistro',
+  'Quintessential',
+  'Jungle',
+  'Sweet',
+];
+
+export const amounts = ['high', 'medium', 'low'];
