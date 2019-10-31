5c841e17d952086f332829e23210f2ed5a44ea34
Corey Angelus <corey.angelus@accenture.com>
2019-10-30 21:50:36 -0400

    add plant type cards

---
+++ client/src/assets/index.js
@@ -0,0 +1,8 @@
+export default {
+  sunLow: require('./sunLow.svg'),
+  sunMedium: require('./sunMedium.svg'),
+  sunHigh: require('./sunHigh.svg'),
+  waterLow: require('./waterLow.svg'),
+  waterMedium: require('./waterMedium.svg'),
+  waterHigh: require('./waterHigh.svg'),
+};
