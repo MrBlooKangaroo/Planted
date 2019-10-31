79b2de4876a596a55aea9e15462a083ccf705ec4
Corey Angelus <corey.angelus@accenture.com>
2019-10-31 10:57:32 -0400

    switch out icons for designers'

--- client/src/assets/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  sunLow: require('./sunLow.svg'),
-  sunMedium: require('./sunMedium.svg'),
-  sunHigh: require('./sunHigh.svg'),
-  waterLow: require('./waterLow.svg'),
-  waterMedium: require('./waterMedium.svg'),
-  waterHigh: require('./waterHigh.svg'),
-};
