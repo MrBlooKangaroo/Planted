5699671d2f9bb099da05881c7b22d9203804d6fe
Corey Angelus <corey.angelus@accenture.com>
2019-10-30 21:50:36 -0400

    add plant type cards

--- client/src/components/Layout/NavBar/index.jsx
+++ client/src/components/Layout/NavBar/index.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import { navBar, logo, navContentRight, login, searchInput } from './navbar.css'
+import React from 'react';
+import {
+  navBar,
+  logo,
+  navContentRight,
+  login,
+  searchInput,
+} from './navbar.css';
 
 const NavBar = () => (
-	<main className={navBar}>
-		<div className={logo}>Planted</div>
-		<div className={navContentRight}>
-			<input
-				className={searchInput}
-				type='text'
-				placeholder=' &#xf002;    Search Plant Names'
-			/>
-			<button className={login}>Log in</button>
-		</div>
-	</main>
-)
+  <main className={navBar}>
+    <div className={logo}>Planted</div>
+    <div className={navContentRight}>
+      <input
+        className={searchInput}
+        type="text"
+        placeholder=" &#xf002;    Search Plant Names"
+      />
+      <button className={login}>Log in</button>
+    </div>
+  </main>
+);
 
-export default NavBar
+export default NavBar;
