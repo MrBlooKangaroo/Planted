a4a68c645c4c09fd5f558df48759d34bc80a17ef
Corey Angelus <corey.angelus@accenture.com>
2019-11-03 23:05:32 -0500

    style nav links and add bold on selection

--- client/src/components/Layout/NavBar/index.jsx
+++ client/src/components/Layout/NavBar/index.jsx
@@ -7,28 +7,28 @@ import {
   navContentRight,
   login,
   searchInput,
-  navContentLeft,
-  routesContainer,
-  navBarRoute,
+  // navContentLeft,
+  navRoutesContainer,
+  navRoute,
+  navRouteSelected,
 } from './navbar.css';
 
-const renderRoute = route => {
-  const navText = route === '/' ? 'EXPLORE' : route.toUpperCase();
+const renderRoute = (route, pathname) => {
+  const navText = route === '/' ? 'EXPLORE' : route.slice(1).toUpperCase();
+  const className = pathname === route ? navRouteSelected : navRoute;
   return (
-    <Link to={route} key={route} id={route} className={navBarRoute}>
+    <Link to={route} key={route} id={route} className={className}>
       {navText}
     </Link>
   );
 };
 
-const NavBar = () => {
+const NavBar = ({ location: { pathname } }) => {
   return (
     <nav className={navBar}>
       <div className={logo}>Planted</div>
-      <div className={navContentLeft}>
-        <div className={routesContainer}>
-          {routes.map(route => renderRoute(route))}
-        </div>
+      <div className={navRoutesContainer}>
+        {routes.map(route => renderRoute(route, pathname))}
       </div>
       <div className={navContentRight}>
         <input
@@ -36,7 +36,7 @@ const NavBar = () => {
           type="text"
           placeholder=" &#xf002;    Search Plant Names"
         />
-        <button className={login}>Log in</button>
+        <button className={login}>Log out</button>
       </div>
     </nav>
   );
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  IndexRoute,
-  Redirect,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import Layout from './Layout';
 import Explore from './Explore';
 import Garden from './Garden';
-import Page404 from './Page404';
 
 const client = new ApolloClient({
   uri: 'https://planted-server.herokuapp.com/graphql',
