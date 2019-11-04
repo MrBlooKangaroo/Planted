c776994a7a716d3d83ecb12be5aba2f21f6c22a7
Corey Angelus <corey.angelus@accenture.com>
2019-11-03 20:25:03 -0500

    pausing rebase to checkout new branch

---
+++ client/src/components/home-page/root/index.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import NavBar from '../NavBar';
+import Explore from '../Explore';
+
+const HomePageRoot = () => (
+  <Fragment>
+    <NavBar />
+    <Explore />
+  </Fragment>
+);
+
+export { HomePageRoot };
