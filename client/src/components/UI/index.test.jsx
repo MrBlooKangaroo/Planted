4fd21aa4081798ed99f9099792deff9bd5759038
Corey Angelus <corey.angelus@accenture.com>
2019-11-07 09:32:36 -0500

    move PlantCardSmall component to UI folder

--- client/src/components/UI/index.test.jsx
+++ client/src/components/UI/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup } from 'react-testing-library';
+import { cleanup } from '@testing-library/react';
 import { mount } from 'enzyme';
 import PlantCardSmall from './PlantCardSmall';
 import styles from './styles.css';
