f87becbb6b40259d9f49dc739f0c6089ceed9b77
Corey Angelus <corey.angelus@accenture.com>
2019-11-12 00:59:03 -0500

    CSS refactor

--- client/src/components/garden-view/NookList/index.jsx
+++ client/src/components/garden-view/NookList/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import NookCardSmall from '../../UI/nook-cards/NookCardSmall';
 import { nooksContainer } from './styles.css';
 
