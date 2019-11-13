97f466a388726eacf6a56d829e91edbf12ad7cc1
Corey Angelus <corey.angelus@accenture.com>
2019-11-13 14:55:49 -0500

    add suggestedPlantTypes query and getNookByUserId query

--- server/resolvers/__tests__/queries/nook-resolver.test.js
+++ server/resolvers/__tests__/queries/nook-resolver.test.js
@@ -1,75 +1,118 @@
-const db = require('../../../models');
-const { cleanUpDb, createQuery, createTestClient, closeDbConnection } = require('../../../utils/testing');
-const { testUser, testNook, testPlant, testPlantType } = require('../../../utils/testing/testData');
+const db = require("../../../models");
+const {
+  cleanUpDb,
+  createQuery,
+  createTestClient,
+  closeDbConnection
+} = require("../../../utils/testing");
+const {
+  testUser,
+  testNook,
+  testPlant,
+  testPlantType
+} = require("../../../utils/testing/testData");
 
 afterEach(cleanUpDb);
 afterAll(closeDbConnection);
 
-describe('Nook Resolver', () => {
-  const query = createQuery(__dirname, '../../../utils/queries/nook.graphql');
+describe("Nook Resolver", () => {
+  describe("when no arguments are passed in", () => {
+    const query = createQuery(__dirname, "../../../utils/queries/nook.graphql");
 
-  it('returns data for the specified nook', async () => {
-    const { testClient } = await createTestClient();
-    const user = await db.user.create(testUser);
-    const nook = await db.nook.create({
-      ...testNook,
-      userId: user.id,
+    it("should return data for the specified nook", async () => {
+      const { testClient } = await createTestClient();
+      const user = await db.user.create(testUser);
+      const nook = await db.nook.create({
+        ...testNook,
+        userId: user.id
+      });
+      const variables = { id: nook.id };
+      const response = await testClient.query({ query, variables });
+      const responseNook = response.data.nook;
+
+      expect(response.errors).toBe(undefined);
+      expect(responseNook).toBeDefined();
+      expect(responseNook.user).toBeDefined();
+      expect(responseNook.name).toBe(testNook.name);
+      expect(responseNook.photoUrl).toBe(testNook.photoUrl);
+      expect(responseNook.luxLevel).toBe(testNook.luxLevel);
     });
-    const variables = { id: nook.id };
-    const response = await testClient.query({ query, variables });
-    const responseNook = response.data.nook;
 
-    expect(response.errors).toBe(undefined);
-    expect(responseNook).toBeDefined();
-    expect(responseNook.user).toBeDefined();
-    expect(responseNook.name).toBe(testNook.name);
-    expect(responseNook.photoUrl).toBe(testNook.photoUrl);
-    expect(responseNook.luxLevel).toBe(testNook.luxLevel);
+    it("should return NOT_FOUND error if invalid nook id supplied", async () => {
+      const { testClient } = await createTestClient();
+      const variables = { id: "0b9f38f1-333f-42db-b0c7-3939cab66bc8" };
+      const response = await testClient.query({ query, variables });
+      const { errors } = response;
+
+      expect(errors.length).toBe(1);
+      expect(errors[0].extensions.code).toBe("NOT_FOUND");
+    });
+
+    it("should include associated plants in the response", async () => {
+      const { testClient } = await createTestClient();
+      const user = await db.user.create(testUser);
+      const plantType = await db.plantType.create(testPlantType);
+      const nook = await db.nook.create({
+        ...testNook,
+        userId: user.id
+      });
+      await db.plant.create({
+        ...testPlant,
+        nookId: nook.id,
+        plantTypeId: plantType.id
+      });
+      const variables = { id: nook.id };
+      const response = await testClient.query({ query, variables });
+      const responseNook = response.data.nook;
+
+      expect(responseNook.plants).toBeDefined();
+      expect(responseNook.plants.length).toBe(1);
+    });
+
+    it("should include associated user in the response", async () => {
+      const { testClient } = await createTestClient();
+      const user = await db.user.create(testUser);
+      const nook = await db.nook.create({
+        ...testNook,
+        userId: user.id
+      });
+      const variables = { id: nook.id };
+      const response = await testClient.query({ query, variables });
+      const responseNook = response.data.nook;
+
+      expect(responseNook.user).toBeDefined();
+      expect(responseNook.user.id).toBe(user.id);
+    });
   });
 
-  it('should return NOT_FOUND error if invalid nook id supplied', async () => {
-    const { testClient } = await createTestClient();
-    const variables = { id: '0b9f38f1-333f-42db-b0c7-3939cab66bc8' };
-    const response = await testClient.query({ query, variables });
-    const { errors } = response;
+  describe("when userId argument is passed in", () => {
+    const query = createQuery(
+      __dirname,
+      "../../../utils/queries/getNooksByUserId.graphql"
+    );
 
-    expect(errors.length).toBe(1);
-    expect(errors[0].extensions.code).toBe('NOT_FOUND');
-  });
+    it("should return data for the specified nook", async () => {
+      const { testClient } = await createTestClient();
+      const user = await db.user.create(testUser);
+      for (let i = 0; i < 5; i++) {
+        await db.nook.create({
+          ...testNook,
+          userId: user.id
+        });
+      }
+      const variables = { userId: user.id };
+      const response = await testClient.query({ query, variables });
+      const responseNooks = response.data.nooks;
 
-  it('should include associated plants in the response', async () => {
-    const { testClient } = await createTestClient();
-    const user = await db.user.create(testUser);
-    const plantType = await db.plantType.create(testPlantType);
-    const nook = await db.nook.create({
-      ...testNook,
-      userId: user.id,
+      expect(response.errors).toBe(undefined);
+      expect(responseNooks).toBeDefined();
+      expect(responseNooks.length).toBe(5);
+      responseNooks.forEach(async responseNook => {
+        expect(responseNook).toBeDefined();
+        expect(responseNook.name).toBe(testNook.name);
+        expect(responseNook.luxLevel).toBe(testNook.luxLevel);
+        expect(responseNook.plants).toBeDefined();
+      });
     });
-    await db.plant.create({
-      ...testPlant,
-      nookId: nook.id,
-      plantTypeId: plantType.id,
-    });
-    const variables = { id: nook.id };
-    const response = await testClient.query({ query, variables });
-    const responseNook = response.data.nook;
-
-    expect(responseNook.plants).toBeDefined();
-    expect(responseNook.plants.length).toBe(1);
-  });
-
-  it('should include associated user in the response', async () => {
-    const { testClient } = await createTestClient();
-    const user = await db.user.create(testUser);
-    const nook = await db.nook.create({
-      ...testNook,
-      userId: user.id,
-    });
-    const variables = { id: nook.id };
-    const response = await testClient.query({ query, variables });
-    const responseNook = response.data.nook;
-
-    expect(responseNook.user).toBeDefined();
-    expect(responseNook.user.id).toBe(user.id);
   });
 });
--- server/utils/queries/index.js
+++ server/utils/queries/index.js
@@ -1,8 +1,10 @@
 module.exports = {
-  user: require('./user.graphql'),
-  plant: require('./plant.graphql'),
-  nook: require('./nook.graphql'),
-  plantType: require('./plantType.graphql'),
-  watering: require('./watering.graphql'),
-  wish: require('./wish.graphql'),
+  user: require("./user.graphql"),
+  plant: require("./plant.graphql"),
+  nook: require("./nook.graphql"),
+  plantType: require("./plantType.graphql"),
+  watering: require("./watering.graphql"),
+  wish: require("./wish.graphql"),
+  suggestedPlantTypes: require("./suggestedPlantTypes.graphql"),
+  getNooksByUserId: require("./getNooksByUserId.graphql")
 };
