022584faabb061254f3e930a903571da35348995
Corey Angelus <corey.angelus@accenture.com>
2019-11-14 15:57:43 -0500

    reset app.jsx

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import Layout from './layout/root';
-import Explore from './explore-page/root';
-import { rootPath } from 'constants/paths';
+import Layout from 'components/Layout/root';
+import Explore from 'components/explore-page/root';
+import {
+  rootPath,
+  nookPath,
+  plantTypePath,
+  wishlistPath,
+} from 'constants/paths';
+import { NookDetail } from '../components/nook-view/root';
+import { PlantTypePage } from './plant-page/root';
+import { WishlistView } from '../components/wishlist-view/root';
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_BACKEND_URL,
@@ -19,6 +27,9 @@ export const App = () => (
       <Switch>
         <Layout>
           <Route component={Explore} exact path={rootPath} />
+          <Route component={NookDetail} path={nookPath} />
+          <Route component={PlantTypePage} path={plantTypePath} />
+          <Route component={WishlistView} exact path={wishlistPath} />
         </Layout>
       </Switch>
     </BrowserRouter>
