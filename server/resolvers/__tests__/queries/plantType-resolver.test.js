b832d7921f9b4f93ccf1e7ebd756e41275d7d504
Corey Angelus <corey.angelus@accenture.com>
2019-11-14 16:27:15 -0500

    reconfigure plantType resolver test

--- server/resolvers/__tests__/queries/plantType-resolver.test.js
+++ server/resolvers/__tests__/queries/plantType-resolver.test.js
@@ -1,30 +1,28 @@
-const db = require("../../../models");
+const db = require('../../../models');
 const {
   cleanUpDb,
   createQuery,
   createTestClient,
-  closeDbConnection
-} = require("../../../utils/testing");
+  closeDbConnection,
+} = require('../../../utils/testing');
 const {
   testUser,
   testPlant,
   testNook,
   testPlantType,
-  testPlantTypeAlt1,
-  testPlantTypeAlt2
-} = require("../../../utils/testing/testData");
+} = require('../../../utils/testing/testData');
 
 afterEach(cleanUpDb);
 afterAll(closeDbConnection);
 
-describe("Plant Type Resolver", () => {
-  describe("when no luxLevel is passed in", () => {
+describe('Plant Type Resolver', () => {
+  describe('when no luxLevel is passed in', () => {
     const query = createQuery(
       __dirname,
-      "../../../utils/queries/plantType.graphql"
+      '../../../utils/queries/plantType.graphql',
     );
 
-    it("returns data for the specified plantType", async () => {
+    it('returns data for the specified plantType', async () => {
       const { testClient } = await createTestClient();
       const plantType = await db.plantType.create(testPlantType);
       const variables = { id: plantType.id };
@@ -36,64 +34,64 @@ describe("Plant Type Resolver", () => {
       expect(responsePlantType.name).toBe(testPlantType.name);
       expect(responsePlantType.photoUrl).toBe(testPlantType.photoUrl);
       expect(responsePlantType.photoUrlHorizontalCrop).toBe(
-        testPlantType.photoUrlHorizontalCrop
+        testPlantType.photoUrlHorizontalCrop,
       );
       expect(responsePlantType.photoUrlVerticalCrop).toBe(
-        testPlantType.photoUrlVerticalCrop
+        testPlantType.photoUrlVerticalCrop,
       );
       expect(responsePlantType.featuresWeb).toBe(testPlantType.featuresWeb);
       expect(responsePlantType.instructionsWeb).toBe(
-        testPlantType.instructionsWeb
+        testPlantType.instructionsWeb,
       );
       expect(responsePlantType.featuresiOS).toBe(testPlantType.featuresiOS);
       expect(responsePlantType.instructionsiOS).toBe(
-        testPlantType.instructionsiOS
+        testPlantType.instructionsiOS,
       );
       expect(responsePlantType.petToxicity).toBe(testPlantType.petToxicity);
       expect(responsePlantType.colorPalette).toBe(testPlantType.colorPalette);
       expect(responsePlantType.humidityAdvice).toBe(
-        testPlantType.humidityAdvice
+        testPlantType.humidityAdvice,
       );
       expect(responsePlantType.travelAdvice).toBe(testPlantType.travelAdvice);
       expect(responsePlantType.careAdvice).toBe(testPlantType.careAdvice);
       expect(responsePlantType.luxLevelInfo).toBe(testPlantType.luxLevelInfo);
       expect(responsePlantType.waterCycleInfo).toBe(
-        testPlantType.waterCycleInfo
+        testPlantType.waterCycleInfo,
       );
       expect(responsePlantType.hasJungleVibes).toBe(
-        testPlantType.hasJungleVibes
+        testPlantType.hasJungleVibes,
       );
       expect(responsePlantType.isAiryFresh).toBe(testPlantType.isAiryFresh);
       expect(responsePlantType.isAirPurifying).toBe(
-        testPlantType.isAirPurifying
+        testPlantType.isAirPurifying,
       );
       expect(responsePlantType.luxLevel).toBe(testPlantType.luxLevel);
       expect(responsePlantType.waterLevel).toBe(testPlantType.waterLevel);
       expect(responsePlantType.waterCycle).toBe(testPlantType.waterCycle);
     });
 
-    it("should return NOT_FOUND error if invalid plantType id supplied", async () => {
+    it('should return NOT_FOUND error if invalid plantType id supplied', async () => {
       const { testClient } = await createTestClient();
-      const variables = { id: "0b9f38f1-333f-42db-b0c7-3939cab66bc8" };
+      const variables = { id: '0b9f38f1-333f-42db-b0c7-3939cab66bc8' };
       const response = await testClient.query({ query, variables });
       const { errors } = response;
 
       expect(errors.length).toBe(1);
-      expect(errors[0].extensions.code).toBe("NOT_FOUND");
+      expect(errors[0].extensions.code).toBe('NOT_FOUND');
     });
 
-    it("should include associated plants in the response", async () => {
+    it('should include associated plants in the response', async () => {
       const { testClient } = await createTestClient();
       const user = await db.user.create(testUser);
       const plantType = await db.plantType.create(testPlantType);
       const nook = await db.nook.create({
         ...testNook,
-        userId: user.id
+        userId: user.id,
       });
       await db.plant.create({
         ...testPlant,
         nookId: nook.id,
-        plantTypeId: plantType.id
+        plantTypeId: plantType.id,
       });
       const variables = { id: plantType.id };
       const response = await testClient.query({ query, variables });
@@ -104,38 +102,43 @@ describe("Plant Type Resolver", () => {
     });
   });
 
-  describe("when a luxLevel variable is passed in", () => {
+  describe('when a luxLevel variable is passed in', () => {
     const query = createQuery(
       __dirname,
-      "../../../utils/queries/getPlantTypeSuggestions.graphql"
+      '../../../utils/queries/getPlantTypeSuggestions.graphql',
     );
 
-    it("returns all plantTypes that have the same luxLevel", async () => {
+    it('returns all plantTypes that have the "HIGH" luxLevel', async () => {
       const { testClient } = await createTestClient();
-      [testPlantType, testPlantTypeAlt1, testPlantTypeAlt2].forEach(
-        async testSeed => {
-          const { luxLevel } = await db.plantType.create(testSeed);
-          const variables = { luxLevel };
-          const response = await testClient.query({ query, variables });
-          const responsePlantTypes = response.data.plantTypes;
-          expect(response.errors).toBe(undefined);
-          switch (luxLevel) {
-            case "HIGH":
-              expect(responsePlantTypes.length).toBe(1);
-              break;
-            default:
-            case "MEDIUM":
-              expect(responsePlantTypes.length).toBe(1);
-              break;
-            case "LOW":
-              expect(responsePlantTypes.length).toBe(1);
-              break;
-          }
-          responsePlantTypes.forEach(responsePlantType => {
-            expect(responsePlantType.luxLevel).toBe(testSeed.luxLevel);
-          });
-        }
-      );
+      const variables = { luxLevel: 'HIGH' };
+      const response = await testClient.query({ query, variables });
+      const responsePlantTypes = response.data.plantTypes;
+      expect(response.errors).toBe(undefined);
+      responsePlantTypes.forEach(responsePlantType => {
+        expect(responsePlantType.luxLevel).toBe('HIGH');
+      });
+    });
+
+    it('returns all plantTypes that have the "MEDIUM" luxLevel', async () => {
+      const { testClient } = await createTestClient();
+      const variables = { luxLevel: 'MEDIUM' };
+      const response = await testClient.query({ query, variables });
+      const responsePlantTypes = response.data.plantTypes;
+      expect(response.errors).toBe(undefined);
+      responsePlantTypes.forEach(responsePlantType => {
+        expect(responsePlantType.luxLevel).toBe('MEDIUM');
+      });
+    });
+
+    it('returns all plantTypes that have the "LOW" luxLevel', async () => {
+      const { testClient } = await createTestClient();
+      const variables = { luxLevel: 'LOW' };
+      const response = await testClient.query({ query, variables });
+      const responsePlantTypes = response.data.plantTypes;
+      expect(response.errors).toBe(undefined);
+      responsePlantTypes.forEach(responsePlantType => {
+        expect(responsePlantType.luxLevel).toBe('LOW');
+      });
     });
   });
 });
