fa64529a7ae33f1d2af36623931b2c8ae761bfdf
Angelus, Corey <corey.angelus@accenture.com>
2019-12-12 17:40:22 -0500

    fix plant type sorting bug

--- client/src/components/explore-page/PlantTypeList/index.jsx
+++ client/src/components/explore-page/PlantTypeList/index.jsx
@@ -4,19 +4,19 @@ import PlantCardLarge from '../../UI/plant-cards/PlantCardLarge';
 
 const PlantTypeList = ({ plantTypes }) => {
   const topThreePlantTypeNames = ['Monstera', 'Elephant Ear', 'Senecio'];
-  const filteredPlantTypes = plantTypes.filter(
-    plantType => !topThreePlantTypeNames.includes(plantType.name),
-  );
+  const filteredPlantTypes = plantTypes.filter(plantType => {
+    return !topThreePlantTypeNames.includes(plantType.name);
+  });
   const topThreePlantTypes = topThreePlantTypeNames.map(plantTypeName =>
     plantTypes.find(p => p.name === plantTypeName),
   );
   const sortedPlantTypeList = topThreePlantTypes.concat(filteredPlantTypes);
-  debugger;
+  const listWithoutUndefined = sortedPlantTypeList.filter(p => p !== undefined);
   return (
     <>
       {plantTypes.length > 0 && (
         <div className={plantTypeList}>
-          {sortedPlantTypeList.map(plantType => (
+          {listWithoutUndefined.map(plantType => (
             <PlantCardLarge key={plantType.name} {...plantType} />
           ))}
         </div>
